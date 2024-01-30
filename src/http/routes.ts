9cf5549267b565f9ff4599477b9c47b544853531
Gustavo-Kissel <kisselgustavo@gmail.com>
2024-01-30 10:09:45 -0300

    gkissel : Test Enviroment

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -7,7 +7,7 @@ import { verifyJWT } from './middlewares/verify-jwt'
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
-  app.post('sessions', authenticate)
+  app.post('/sessions', authenticate)
 
   // Authenticated routes
   app.get('/me', { onRequest: [verifyJWT] }, profile)
